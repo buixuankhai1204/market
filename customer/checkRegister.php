ac9bcfc45a31ff90f26ed9379111919532c4d5a9
vovankhoa120401 <khoavo120401@gmail.com>
2021-09-23 10:56:07 +0700

    sua customor 1

--- customer/checkRegister.php
+++ customer/checkRegister.php
@@ -38,66 +38,66 @@ class checkRegister {
 
     public function checkRegister(checkRegister $checkRegister){
         try {
-
+            $error_code = array();
             if($checkRegister->userName === "" || $checkRegister->fullName === "" || $checkRegister->password === "" || $checkRegister->address === "" || $checkRegister->phone_number === "" || $checkRegister->birthday === "" || $checkRegister->created_at ==="" || $checkRegister->updated_at === "" || $checkRegister -> active_status === ""){
-                $array_respone = [
-                    "success" => false,
-                    "status_code" => 100,
-                    "message" => "xin điền đầy đủ thông tin",
-                    "error" => true,
-                ];
-                echo json_encode($array_respone);
+                $error_register = "xin nhập đầy đủ tất cả thông tin";
+
+                $error_code = array(
+                    "message" => $error_register,
+                );
+                echo json_encode($error_code);
             }
             else{
                 $parttern = "/^[A-Za-z0-9_\.]{6,32}$/";
                 if(!preg_match($parttern, $_POST['userName'])){
-                    $array_respone = [
-                        "success" => false,
-                        "status_code" => 100,
-                        "message" => "xin nhập tài khoản đúng định dạng",
-                        "error" => true,
-                    ];
-                    echo json_encode($array_respone);
+                    $error_userName = "xin nhập tài khoản đúng định dạng";
+                    $error_code = array(
+                        "username" => $error_userName,
+                    );
+                    echo json_encode($error_code);
                 }
                 
                 $parttern1 = "/^([A-Z]){1}([\w_\.!@#$%^&*()]+){5,31}$/";
                 if(!preg_match($parttern1, $_POST['password'])){
-                    $array_respone = [
-                        "success" => false,
-                        "status_code" => 100,
-                        "message" => "xin nhập ,ật khẩu đúng định dạng",
-                        "error" => true,
-                    ];
-                    echo json_encode($array_respone);
+                    $error_password = "xin nhập mật khẩu đúng định dạng";
+                    $error_code = array(
+                        "message" => $error_password,
+                    );
+                    echo json_encode($error_code);
+                } else{ // tạo mật khẩu
+                    $password_hash = md5($checkRegister->password);
+                    $query = "SELECT UserName,FullName,Password FROM customers WHERE UserName = $checkRegister->userName";
+                    $row = json_decode(responeCheckQuery($query));
+                    if($password_hash == $row -> data[0] ->password){
+                        $_SESSION['info_customer']['userName'] = $row->data[0]->userName;
+                        $_SESSION['info_customer']['fullname'] = $row->data[0]->FullName;
+                    }
                 }
                 
                 $parttern2 = "/^[A-Za-z0-9_.]{6,32}@([a-zA-Z0-9]{2,12})(.[a-zA-Z]{2,12})+$/";
                 if(!preg_match($parttern2, $_POST['password'])){
-                    $array_respone = [
-                        "success" => false,
-                        "status_code" => 100,
-                        "message" => "xin nhập email đúng định dạng",
-                        "error" => true,
-                    ];
-                    echo json_encode($array_respone);
+                    $error_password = "xin nhập email đúng định dạng";
+                    $error_code = array(
+                        "message" => $error_password,
+                    );
+                    echo json_encode($error_code);
                 }
-                
+                // viet ham trong myhelper  2 tham so dau vao la username va password, xu ly va trar ve hash password
                 $data = '+11234567890';
                 if(!preg_match( '/^\+\d(\d{3})(\d{3})(\d{4})$/', $data,  $matches ) )
                 {
-                    $array_respone = [
-                        "success" => false,
-                        "status_code" => 100,
-                        "message" => "xin nhập số điện thoại đúng định dạng",
-                        "error" => true,
-                    ];
-                    echo json_encode($array_respone);
+                    $error_sdt = "xin nhập mật khẩu đúng định dạng";
+                    $error_code = array(
+                        "message" => $error_sdt,
+                    );
+                    echo json_encode($error_code);
                 }
                 else{
                     $result = $matches[1] . '-' .$matches[2] . '-' . $matches[3];
                     return $result;
                 }
             }
+         
             
         } catch (\Throwable $th) {
             //throw $th;
